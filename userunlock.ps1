7c8abcaa571be22528f8cdd6008074df44157c34
Zyan Shull-Bain <shullbaz@oregonstate.edu>
2026-02-18 10:50:06 -0800

    Unlock user script 2

--- userunlock.ps1
+++ userunlock.ps1
@@ -1,6 +1,34 @@
+# Import-Module ActiveDirectory
+
+# Search-ADAccount -LockedOut | ForEach-Object {
+#     Unlock-ADAccount -Identity $_.SamAccountName
+#     Write-Host "Unlocked $($_.SamAccountName)"
+# }
+
+# Import Active Directory module
 Import-Module ActiveDirectory
 
-Search-ADAccount -LockedOut | ForEach-Object {
-    Unlock-ADAccount -Identity $_.SamAccountName
-    Write-Host "Unlocked $($_.SamAccountName)"
+# Define users 
+$Users = @(
+    "dmitchell",
+    "brodriguez",
+    "bdavis",
+    "dthompson",
+    "eparker",
+    "rwilliams",
+    "afoster",
+    "janderson",
+    "lnguyen",
+    "clee",
+    "rmartinez",
+    "kbrown",
+    "mdavis",
+    "dharris",
+    "jtaylor"
+)
+
+# Unlock each account
+foreach ($User in $Users) {
+    Unlock-ADAccount -Identity $User
+    Write-Host "Unlocked account: $User"
 }
